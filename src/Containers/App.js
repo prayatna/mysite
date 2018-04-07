0a474c1ae68f2c8b46ba5a54641646c2ff9159b7
Prayatna <prayatna.bhattarai@gmail.com>
2018-04-08 00:01:26 +1000

    restrucure

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,21 +3,15 @@ import './App.css';
 import Intro from '../Components/Intro/Intro';
 import Projects from '../Components/Projects/Projects';
 import Contact from '../Components/Contact/Contact';
-import {AnimatedSwitch} from 'react-router-transition';
-import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
                 <div className="App">
-
-
                     <Intro/>
-                    <Projects/>
-                    <Contact/>
+                    {/*<Projects/>*/}
+                    {/*<Contact/>*/}
                 </div>
-            </BrowserRouter>
         );
     }
 }
